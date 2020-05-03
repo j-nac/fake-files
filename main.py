87eed450db5baa9cbb5297c6cb03068eb77e77bc
j-nac <63821647+j-nac@users.noreply.github.com>
2020-05-03 16:56:50 -0700

    Generate files function created.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from random import choice, randint
+
 characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
               'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', '\\', ';', '\'', ',', '.', '/', '`', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '{', '}', '|', ':', '"', '<', '>', '?', '~']
 
 
-def gen_data(bytes):
+def gen_data(size):
     data = []
-    for byte_count in range(bytes):
+    for x in range(size):
         rand_byte = choice(characters)
         data.append(rand_byte)
     return ''.join(data)
@@ -22,5 +23,36 @@ def gen_filename(names_array, extensions):
     return f'out_files/{filename}.{extension}'
 
 
-print(gen_filename([['dog', 'cat'], ['blue', 'red']], ['txt', 'png']))
-print(gen_data(100))
+def gen_file(names_array, extensions, size_range, count):
+    used_names = []
+    for i in range(count):
+        while True:
+            name = gen_filename(names_array, extensions)
+            if not(name in used_names):
+                used_names.append(name)
+                break
+        size = randint(size_range[0], size_range[1])
+        data = gen_data(size)
+        out_file = open(name, 'w+')
+        out_file.write(data)
+        out_file.close
+
+
+'''
+#===== Example =====
+
+names_array = [['blue', 'red', 'green', 'yellow'],
+               ['Tim', 'John', 'Aaron'], ['hello', 'goodbye']]
+
+file_types = ['txt', 'docx', 'odt', 'doc', 'png', 'jpg', 'mp3', 'mp4']
+
+file_size = 1000000
+
+file_count = 5
+
+
+gen_file(names_array, file_types, file_size, file_count)
+
+#===== Enjoy! =====
+
+'''
